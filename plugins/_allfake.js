75a87e05a80bc6b5ba5a1cb24bbe0605fac36d9b
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-08-23 11:22:44 +0800

    New
    Nee

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -11,7 +11,28 @@ handler.all = async function (m, { conn, text, args, usedPrefix, command }) {
 	  
   // Fake Random
         let pft = ["fimg","fimgv","fpayment","ftroli","fkontak","fvn","fvid","ftextt","fliveLoc","fliveLoc2","ftoko","fdocs","fgclink","fgif"]
-		global.fakes = pft.getRandom()
+		let pdoc = ["application/vnd.openxmlformats-officedocument.presentationml.presentation","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","application/vnd.openxmlformats-officedocument.wordprocessingml.document","application/vnd.ms-excel","application/msword","application/pdf","text/rtf"]
+		// Fake Knights
+		let knights = await(await import('knights-canvas'))
+		let imagea = await new knights.Jo()
+    .setImage(pp)
+    .toBuild();
+  let dataa = imagea.toBuffer();
+  let imageb = await new knights.Patrick()
+    .setAvatar(pp)
+    .toAttachment();
+  let datab = imageb.toBuffer();
+  let imagec = await new knights.Bonk()
+    .setAvatar1(pp)
+    .setAvatar2(pp)
+    .toBuild();
+  let datac = imagec.toBuffer();
+  let imaged = await new knights.Burn()
+    .setAvatar(pp)
+    .toAttachment();
+  let datad = imaged.toBuffer();
+  let kn =  [dataa, datab, datac, datad]
+  
         
 	try {
 		pp = await this.profilePictureUrl(m.sender, 'image')
@@ -20,8 +41,9 @@ handler.all = async function (m, { conn, text, args, usedPrefix, command }) {
 	} finally {
 		
         // Doc
-        let pdoc = ["application/vnd.openxmlformats-officedocument.presentationml.presentation","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","application/vnd.openxmlformats-officedocument.wordprocessingml.document","application/vnd.ms-excel","application/msword","application/pdf","text/rtf"]
-		global.doc = pdoc.getRandom()
+        global.doc = pdoc.getRandom()
+		global.fakes = pft.getRandom()
+		global.knimg = kn.getRandom()
 		
 		// Module 
 		global.fetch = import('node-fetch')
@@ -229,33 +251,11 @@ handler.all = async function (m, { conn, text, args, usedPrefix, command }) {
       jpegThumbnail: fs.readFileSync('./thumbnail.jpg'),
       viewOnce: true
     }
+   }
   }
-  }
- 
-		// Fake Knights
-		let knights = await(await import('knights-canvas'))
-		let imagea = await new knights.Jo()
-    .setImage(pp)
-    .toBuild();
-  let dataa = imagea.toBuffer();
-  let imageb = await new knights.Patrick()
-    .setAvatar(pp)
-    .toAttachment();
-  let datab = imageb.toBuffer();
-  let imagec = await new knights.Bonk()
-    .setAvatar1(pp)
-    .setAvatar2(pp)
-    .toBuild();
-  let datac = imagec.toBuffer();
-  let imaged = await new knights.Burn()
-    .setAvatar(pp)
-    .toAttachment();
-  let datad = imaged.toBuffer();
-  let kn =  [dataa, datab, datac, datad]
-  global.knimg = kn.getRandom()
-		
-	}
+ }
 }
+export default handler 
 
 function ucapan() {
     const time = moment.tz('Asia/Jakarta').format('HH')
